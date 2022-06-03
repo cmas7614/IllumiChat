94692c1892299b7f68bafd2e24a6c28922452abf
C <100403119+cmas7614@users.noreply.github.com>
2022-06-03 08:00:36 +0200

    Update post.php

--- post.php
+++ post.php
@@ -1,6 +1,16 @@
 <?php
 session_start();
 if(isset($_SESSION['name'])){
+    if ($admin = true;) {
+        
+    $text = $_POST['text'];
+     
+    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name-admin'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+        
+    }
+    else {
+        
     $text = $_POST['text'];
      
     $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
